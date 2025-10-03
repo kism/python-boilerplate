6a45d0cd9cddb5006d42802297c1aa78672015db
Kieran Gee <kieran.lost.the.game@gmail.com>
2025-10-03 14:38:11 +0800

    fix ci

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,3 +1,4 @@
+import argparse
 import logging
 
 import pytest
@@ -5,8 +6,15 @@ import pytest
 from my_cool_app import __main__
 
 
-def test_main(caplog: pytest.LogCaptureFixture) -> None:
+def test_main(
+    caplog: pytest.LogCaptureFixture,
+    monkeypatch: pytest.MonkeyPatch,
+) -> None:
     caplog.set_level(logging.INFO)
+
+    mock_args = argparse.Namespace(v=0, message="Hello, World!")
+    monkeypatch.setattr(argparse.ArgumentParser, "parse_args", lambda self: mock_args)
+
     __main__.main()
     assert "Hello, World!" in caplog.text
     assert "my-cool-app version: v0.0.1" in caplog.text
